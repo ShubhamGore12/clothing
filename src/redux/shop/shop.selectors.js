ca49ade27686d407e4386e11dd0c37c157d03904
shubham <shubh.shubham21@gmail.com>
2021-06-29 17:39:06 +0530

    add withSpinner HOC

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -11,7 +11,8 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
 	[selectCollections],
-	(collections) => Object.keys(collections).map((key) => collections[key])
+	(collections) =>
+		collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
 // export const selectCollection = collectionUrlParam =>
@@ -21,8 +22,7 @@ export const selectCollectionsForPreview = createSelector(
 //   );
 
 export const selectCollection = memoize((collectionUrlParam) =>
-	createSelector(
-		[selectCollections],
-		(collections) => collections[collectionUrlParam]
+	createSelector([selectCollections], (collections) =>
+		collections ? collections[collectionUrlParam] : null
 	)
 );
